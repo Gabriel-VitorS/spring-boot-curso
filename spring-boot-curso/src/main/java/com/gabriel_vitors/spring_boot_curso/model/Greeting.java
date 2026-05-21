55f9889f05691f4f06efc341942387c52bbf5b06
Gabriel <gabrielvitordasilva@gmail.com>
2026-05-21 13:10:01 -0300

    sessao 4

---
+++ spring-boot-curso/src/main/java/com/gabriel_vitors/spring_boot_curso/model/Greeting.java
@@ -0,0 +1,5 @@
+package com.gabriel_vitors.spring_boot_curso.model;
+
+public record Greeting(long id, String content) {
+    
+}
---
+++ spring-boot-curso/src/test/java/com/gabriel_vitors/spring_boot_curso/StartupTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_vitors.spring_boot_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
